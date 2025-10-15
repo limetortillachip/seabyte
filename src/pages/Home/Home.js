3112b0a89dae5e2529e47007d741ed945d1c47f8
limetortillachip <shanonps@icloud.com>
2025-10-14 21:43:28 -0400

    Add Hero section to homepage

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <h2>Home</h2>;
+  return (
+    <article className="hero">
+      <h2>Welcome to my page!</h2>
+      <div>
+        Hi! My name is Shanon. I am a lover of colorfully styled webpages.
+      </div>
+    </article>
+  );
 }
